25e9bf31f9348a39d2da56ed78ea418a36570b50
MathiasIZZ <mathias.guiguen@gmail.com>
2021-01-14 16:38:48 +0100

    ok anim

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { gsap, TimelineMax } from 'gsap';
+import { gsap, TimelineMax, TweenMax, Elastic } from 'gsap';
 
 
-gsap.registerPlugin(TimelineMax);
+gsap.registerPlugin(TimelineMax, TweenMax, Elastic);
 
 
 
@@ -15,11 +15,13 @@ export class IntroComponent implements OnInit {
 
   TL = new TimelineMax({paused: true});
 
+
   constructor() { }
 
   ngOnInit(): void {
 
     this.createAnim();
+    this.anim1();
   }
 
 
@@ -27,9 +29,20 @@ export class IntroComponent implements OnInit {
   createAnim() {
 
     const titre = document.querySelectorAll('h2');
-
     this.TL.from(titre[0], 1, {y: -100, opacity: 0}).from(titre[1], 2, {y: -100, opacity: 0});
     this.TL.play();
+  }
+
+  anim1() {
+
+    gsap.to('.monBouton2', {
+      duration: 3,
+      scale: 1,
+      y: 25,
+      ease: 'elastic(15, 0.3)'
+    });
+
+
   }
 
 
--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
