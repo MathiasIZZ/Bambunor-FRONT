2075c19bf859716b6530fd0cf09b742af9f098f4
MathiasIZZ <mathias.guiguen@gmail.com>
2021-01-14 18:15:42 +0100

    ok

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {gsap, TimelineMax} from 'gsap';
 
 
+
+gsap.registerPlugin(TimelineMax);
 
 @Component({
   selector: 'app-presentation',
@@ -9,12 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationComponent implements OnInit {
 
-
+    TL = new TimelineMax({paused: true });
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createAnim();
+  }
 
 
+  // tslint:disable-next-line:typedef
+  createAnim() {
+    const titre = document.querySelectorAll('.anime');
+    this.TL.from(titre[0], 3, {y: -100, opacity: 0});
+    this.TL.play();
+  }
 
 }
